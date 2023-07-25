5aea513108fb359369041aefe11a269ad6948e87
SreelathaKanchireddy <105054104+SreelathaKanchireddy@users.noreply.github.com>
2023-07-25 17:28:54 +0530

    first commit

---
+++ SampleOttApp/SampleOttApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SampleOttApp
+//
+//  Created by Kanchireddy sreelatha on 25/07/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
